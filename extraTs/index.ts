159d226095e5b3acaf9daf001fe1e52e529f6f2d
rrishabhrg <rishabh.gupta@successive.tech>
2019-04-25 10:56:59 +0530

    docs: add ts files

---
+++ extraTs/index.ts
@@ -0,0 +1,43 @@
+import {displayPyramid} from './patterns';
+import {displayEquiTriangle} from './patterns';
+import {hasPermission} from './utils';
+import {validateUsers} from './utils/validation/validation'
+
+displayPyramid(5);
+displayEquiTriangle(5);
+
+console.log('.............................................................................');
+console.log(hasPermission("getUsers","trainer","write"));          //true
+console.log(hasPermission("getUsers","trainer","read"));           //true
+console.log(hasPermission("getUsers","trainer","delete"));         //false
+console.log(hasPermission("getUsers","trainee","write"));          //false
+console.log(hasPermission("getUsers","trainee","read"));           //true
+console.log(hasPermission("getUsers","trainee","delete"));         //false
+console.log(hasPermission("getUsers","head-trainer","write"));     //true
+console.log(hasPermission("getUsers","head-trainer","read"));      //true
+console.log(hasPermission("getUsers","head-trainer","delete"));    //true
+
+const users = [     
+    {
+      traineeEmail: 'trainee2@successive.tech@.tech', //FALSE
+      reviewerEmail: 'reviewer2@successive.tech',       
+    },
+  
+    {
+      traineeEmail: 'trainee3@successsive.tech@.tech', //FALSE
+      reviewerEmail: 'reviewer3@successive.tech',
+    },
+  
+    {
+      traineeEmail: 'trainee4@successive.tech',
+      reviewerEmail: 'reviewer4@successive.tech',
+    },
+  
+    {
+      traineeEmail: 'trainee5@successive.tech',
+      reviewerEmail: 'reviewer5@successive@successive.tech', //FALSE
+    }
+  ]
+
+console.log('.............................................................................');
+validateUsers(users);
---
+++ extraTs/utils/index.ts
@@ -0,0 +1,3 @@
+import {default as hasPermission} from './permissions/permissions';
+
+export {hasPermission};
