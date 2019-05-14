0f84a3e1ec94e49f10897908c54ab0c7a087031e
rrishabhrg <rishabh.gupta@successive.tech>
2019-05-14 17:01:20 +0530

    feat: add feature

--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -11,36 +11,6 @@ class userRepository extends versionableRepository {
     return await UsersModel.countDocuments(query);
   }
 
-  // public static generateObjectId(){
-  //   return mongoose.Types.ObjectId();
-  // }
-
-  // public createUser(data: any){                         //CREATE
-  //   const id = userRepository.generateObjectId();
-  //   const model = new UsersModel({
-  //     _id: id,
-  //     ...data
-  //   });
-  //   model.save().then(data => {
-  //     return data;
-  //   })
-  //   .catch(error =>{
-  //     return error.message || "Some error occurred while creating the record."
-  //   });
-  // }
-
-  // public getUserById(id: any){                          //READ
-  //   return UsersModel.findById(id);
-  // }
-
-  // public updateUserById(id: any){                       //UPDATE
-  //   return UsersModel.findByIdAndUpdate();
-  // }
-
-  // public deleteUserById(id: any){                       //DELETE
-  //   return UsersModel.findByIdAndRemove(id);
-  // }
-
   public async signIn(query: any){
     const result = await UsersModel.find(query, { password: 0 });
     return result;
